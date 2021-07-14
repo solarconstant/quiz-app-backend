8f1ca3bf7f40fd92d8562bc12f44a3cfb9477b84
Shubh Ashish <ashishshubh001@gmail.com>
2021-07-14 09:51:26 +0530

    Add Message Sending Time

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+const socketio = require("socket.io");
+const http = require("http");
+
+const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
 
 require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(morgan("dev"));
+app.use(cors());
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}?retryWrites=true&w=majority`, {
     useUnifiedTopology: true,
@@ -15,12 +24,53 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PA
 .then(() => console.log('DB connected successfully.'))   
 .catch(err => console.log('DB connection failed: ', err));
 
-app.use(morgan("dev"));
-app.use(cors());
+io.on('connect', (socket) => {
+socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    let today = new Date();
+    let msg_send_time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
+    socket.emit('message', { user: 'Quizmaster', text: `Hey ${user.name}!, welcome to ${user.room}.`, time: msg_send_time});
+    socket.broadcast.to(user.room).emit('message', { user: 'Quizmaster', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+});
+
+socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    let today = new Date();
+    let msg_send_time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
+    io.to(user.room).emit('message', { user: user.name, text: message, time: msg_send_time});
+
+    callback();
+});
+
+socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+    io.to(user.room).emit('message', { user: 'Quizmaster', text: `${user.name} has left.` });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+})
+});
+
+app.get('/', (req, res) =>
+{
+    res.send("Server for Quiz App.");
+});
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () =>
+server.listen(port, () =>
 {
     console.log(`Server is up and running at port ${port}`);
 })
